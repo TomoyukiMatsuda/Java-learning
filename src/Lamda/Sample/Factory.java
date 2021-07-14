e1a13910393bbe9bfff58e60737a734f24896fa0
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-15 08:40:37 +0900

    関数型インターフェースとラムダ式

--- src/Lamda/Sample/Factory.java
+++ src/Lamda/Sample/Factory.java
@@ -5,15 +5,19 @@ public class Factory {
   public static Sample create() {
     // InnerSampleクラスは static ではないので、インスタンスから生成する
     //return new Factory().new InnerSample();
+  
+    // ラムダ式　Sample型をリターンすることが決まっている、かつ Sample は関数型インターフェース（実装メソッドを一つしか持っていない）
+    // だから、execute() の処理内容だけわかればOKの状態
+    return () -> System.out.println("ラムダ式");
     
     // インターフェース をインスタンス化
-    return new Sample() {
+    //return new Sample() {
       // Sampleインターフェースをimplementsした無名クラスのインスタンスの参照を扱うイメージ。インスタンス化の使い捨て？
-      @Override
-      public void execute() {
-        System.out.println("匿名クラス");
-      }
-    };
+//      @Override
+//      public void execute() {
+//        System.out.println("匿名クラス");
+//      }
+//    };
   }
   
   // Factoryクラスからしかアクセスできない
