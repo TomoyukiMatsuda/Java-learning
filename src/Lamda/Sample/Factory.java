55336333473c92d2ed60706c3a12b897ac06247f
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-15 08:27:47 +0900

    ラムダ式 インターフェースのインスタンス化、匿名クラス

--- src/Lamda/Sample/Factory.java
+++ src/Lamda/Sample/Factory.java
@@ -4,14 +4,23 @@ public class Factory {
   // Factoryパターン Sample 型の SampleImpl インスタンスを返す
   public static Sample create() {
     // InnerSampleクラスは static ではないので、インスタンスから生成する
-    return new Factory().new InnerSample();
+    //return new Factory().new InnerSample();
+    
+    // インターフェース をインスタンス化
+    return new Sample() {
+      // Sampleインターフェースをimplementsした無名クラスのインスタンスの参照を扱うイメージ。インスタンス化の使い捨て？
+      @Override
+      public void execute() {
+        System.out.println("匿名クラス");
+      }
+    };
   }
   
   // Factoryクラスからしかアクセスできない
-  private class InnerSample implements Sample {
-    @Override
-    public void execute() {
-      System.out.println("インナークラス");
-    }
-  }
+//  private class InnerSample implements Sample {
+//    @Override
+//    public void execute() {
+//      System.out.println("インナークラス");
+//    }
+//  }
 }
