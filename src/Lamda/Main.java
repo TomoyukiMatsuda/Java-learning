370afcb93674db299c8801a3d95d717b5cf63869
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-07-20 19:12:30 +0900

    セーブメソッドを実行

--- src/Lamda/Main.java
+++ src/Lamda/Main.java
@@ -3,21 +3,29 @@ package Lamda;
 import Lamda.Sample.Factory;
 import Lamda.Sample.Sample;
 
-// todo ラムダ式5の途中から開始、Function、Consumerとか
-//  https://www.youtube.com/watch?v=xj-IGnqH4EY&list=PL0BiAlg8j4Zu_XVmBIq2DTSP8LdRWSjwV&index=5
+// todo ラムダ式8
+//  https://www.youtube.com/watch?v=xj-IGnqH4EY&list=PL0BiAlg8j4Zu_XVmBIq2DTSP8LdRWSjwV&index=8
 // Mainクラスは Sample しか見ないようにしたい
 public class Main {
   public static void main(String[] args) {
     Test test = new Test();
     // 可変の処理だけをラムダ式で引数に渡している
     test.process(() -> System.out.println("メインメソッドから実行"));
-    
 //    test.process(new Sample() {
 //      @Override
 //      public void execute() {
 //        System.out.println("メインメソッドから実行");
 //      }
 //    });
+    
+    // チェーンで書いて save メソッドを呼ぶだけ
+    Item item = new Item()
+      .id("100")
+      .name("アイテム")
+      .description("アイテムの説明")
+      .price(200);
+    
+    Item.save(item);
   }
 }
 
@@ -27,5 +35,46 @@ class Test {
     System.out.println("スタート");
     sample.execute(); // ここの処理だけ可変としたい
     System.out.println("エンド");
+  }
+}
+
+class Item {
+  private String id;
+  private String name;
+  private String description;
+  private int price;
+  
+  public Item id(String id) {
+    this.id = id;
+    return this;
+  }
+  
+  public Item name(String name) {
+    this.name = name;
+    return this;
+  }
+  
+  public Item description(String description) {
+    this.description = description;
+    return this;
+  }
+  
+  public Item price(int price) {
+    this.price = price;
+    return this;
+  }
+  
+  public static void save(Item item) {
+    System.out.println("セーブ：" + item);
+  }
+  
+  @Override
+  public String toString() {
+    return "Item{" +
+      "id='" + id + '\'' +
+      ", name='" + name + '\'' +
+      ", description='" + description + '\'' +
+      ", price=" + price +
+      '}';
   }
 }
